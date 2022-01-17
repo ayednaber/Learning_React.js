00971177ec1415df0f6ff2ad9d7449a8541664d7
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-17 11:02:18 -0500

    Added comments to first JS file

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+// After we imported React, ReactDOM, and Babel in our head in the HTML document,
+// I am using the render method from our global ReactDOM variable to render an element.
+// First parameter is what I want to render, and the second one is where I want to render it.
+
 ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
