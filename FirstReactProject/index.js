2768bf833f375228a2b4c3265293147d4f2ee41d
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-17 13:24:47 -0500

    Created MainContent element and rendered it using ReactDOM

--- FirstReactProject/index.js
+++ FirstReactProject/index.js
@@ -4,4 +4,16 @@
 // I am using the render method from our global ReactDOM variable to render an element.
 // First parameter is what I want to render, and the second one is where I want to render it.
 
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+
+// Here, we are covering the concept of composability, see how we created our own function
+// think of it as OOP but for web development.
+function MainContent() {
+    return (
+        <h1>I am Learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+    <MainContent />, document.getElementById("root"));
