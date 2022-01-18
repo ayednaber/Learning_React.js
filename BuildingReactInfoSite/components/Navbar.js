1c8ae90c27bcfd0b85a083a444e8714a634bc1a8
Ayed Naber <nabera@mcmaster.ca>
2022-01-17 21:48:20 -0500

    Completed Project 1 for this course

--- BuildingReactInfoSite/components/Navbar.js
+++ BuildingReactInfoSite/components/Navbar.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src="../images/react-icon-small.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
