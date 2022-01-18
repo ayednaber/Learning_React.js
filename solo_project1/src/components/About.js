e698d9abb105e32cdad51cfa3e2550e5baffbab7
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-18 07:11:02 -0500

    Added About and Interests components

---
+++ solo_project1/src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div className="about-section">
+            <h2>About</h2>
+            <p>I am a third-year Computer Science student at McMaster University. I have a particular interest in
+                web development. I am currently working as a developer at RBC Wealth Management, and I
+                hope to get the most of my experience here.
+            </p>
+        </div>
+    )
+}
---
+++ solo_project1/src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className="interests-section">
+            <h2>Interests</h2>
+            <p>Football. Basketball. Cooking. Listening to music. Web development. Avid reader and learner.</p>
+        </div>
+    )
+}
