12a95240c9622330a6dffa2fc8d2b4873b724461
Ayed Naber <nabera@mcmaster.ca>
2022-01-17 20:23:25 -0500

    Solving part 2 of the custom components challenge

--- CustomComponents/index.js
+++ CustomComponents/index.js
@@ -5,18 +5,39 @@
 // Here, I created a component called "Reason", and then I rendered it inside the root
 // element in my HTML page.
 
+/**
+ Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+
+ */
+
 import React from "react"
 import ReactDOM from "react-dom"
 
 function Reason() {
     return (
         <>
+        <header>
+          <nav>
+            <img src="./react-logo.png" width="40px" />
+          </nav>
+        </header>
             <h1>Why I am excited to Learn React!</h1>
             <ol>
                 <li>It is a skill that is in-demand.</li>
                 <li>It allows me to create websites and applications with greater flexibility than Vanilla JavaScript</li>
                 <li>I love learning new skills</li>
             </ol>
+            <footer> 2022 Ayed Naber. All rights reserved</footer>
         </>
     )
 }
