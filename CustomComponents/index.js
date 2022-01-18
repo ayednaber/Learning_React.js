a97014dd57da81b7744c81fc85a72109769d393c
Ayed Naber <nabera@mcmaster.ca>
2022-01-17 20:38:42 -0500

    Learning nesting custom components in one component (composability)

--- CustomComponents/index.js
+++ CustomComponents/index.js
@@ -23,21 +23,40 @@ Part 2:
 import React from "react"
 import ReactDOM from "react-dom"
 
-function Reason() {
-    return (
-        <>
-        <header>
+function Header() {
+  return (
+    <header>
           <nav>
             <img src="./react-logo.png" width="40px" />
           </nav>
         </header>
-            <h1>Why I am excited to Learn React!</h1>
-            <ol>
+  )
+}
+
+function OrderedList() {
+  return (
+    <ol>
                 <li>It is a skill that is in-demand.</li>
                 <li>It allows me to create websites and applications with greater flexibility than Vanilla JavaScript</li>
                 <li>I love learning new skills</li>
             </ol>
-            <footer> 2022 Ayed Naber. All rights reserved</footer>
+  )
+}
+
+function Footer() {
+  return (
+    <footer> 2022 Ayed Naber. All rights reserved</footer>
+  )
+}
+
+// Notice here how we are using the components defined above inside here. This is an example of the composability that we have access to in React.
+function Reason() {
+    return (
+        <>
+        <Header />
+            <h1>Why I am excited to Learn React!</h1>
+            <OrderedList />
+            <Footer />
         </>
     )
 }
