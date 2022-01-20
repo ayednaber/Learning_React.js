fb15cb130e767d34f3083b3a619e627cf97929aa
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-20 13:18:48 -0500

    Added props to Location and made it dynamic (related to Data.js)

--- solo_project2/src/App.js
+++ solo_project2/src/App.js
@@ -1,11 +1,21 @@
 import Navbar from "./components/Navbar";
 import Location from "./components/Location";
+import data from "./data";
 
 function App() {
+  let locationElements = data.map(locationElem => {
+    return (
+    <Location 
+      key={locationElem.id}
+      locationElem={locationElem} // I reached here (TODO now: fix Location.js)
+    />
+    )
+  })
   return (
+
     <div className="app">
     <Navbar />
-    <Location />
+    {locationElements}
    </div>
   )
 }
