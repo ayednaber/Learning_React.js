0ea73365942f1f3868c989f6f17d416144e05744
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-21 15:46:22 -0500

    Added useState for image URL (image src changes onClick of button)

--- meme_generator/src/components/Form.js
+++ meme_generator/src/components/Form.js
@@ -3,18 +3,29 @@ import { FaImage } from "react-icons/fa"
 import memesData from "../memesData"
 
 export default function Form() {
-    function getRandomURL() {
-        const memes = memesData.data.memes
-        let randomEntry = memes[Math.floor(Math.random()*memes.length)]
-        console.log(randomEntry.url)
+    const memes = memesData.data.memes
+    let randomEntry = memes[Math.floor(Math.random()*memes.length)]
+    const [memeImage, updateMemeImage] = React.useState(randomEntry.url)
+
+
+    function getRandomImage() {
+        randomEntry = memes[Math.floor(Math.random()*memes.length)]
+        updateMemeImage(prevImage => randomEntry.url)
+        // console.log(randomEntry.url)
     }
+
+    
     return (
-        <div className="meme-form">
-            <div className="input-text">
-                <input type="text" className="top-text" placeholder="Enter top text..."/>
-                <input type="text" className="bottom-text" placeholder="Enter bottom text..."/>
+        <div className="meme-container">
+            <div className="meme-form">
+                <div className="input-text">
+                    <input type="text" className="top-text" placeholder="Enter top text..."/>
+                    <input type="text" className="bottom-text" placeholder="Enter bottom text..."/>
+                </div>
+                <button onClick={getRandomImage} className="get-meme-button">Get a new meme image <FaImage /></button>
             </div>
-            <button onClick={getRandomURL} className="get-meme-button">Get a new meme image <FaImage /></button>
+            <img src={memeImage} className="meme-image" />
         </div>
+        
     )
 }
