d42d8b329b9f877cc86a0e224cd6a6c36e0f34a5
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-21 11:08:32 -0500

    Practicing onClick event listener by logging random URL when clicking button

--- meme_generator/src/components/Form.js
+++ meme_generator/src/components/Form.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { FaImage } from "react-icons/fa"
+import memesData from "../memesData"
 
 export default function Form() {
+    function getRandomURL() {
+        const memes = memesData.data.memes
+        let randomEntry = memes[Math.floor(Math.random()*memes.length)]
+        console.log(randomEntry.url)
+    }
     return (
-        <form className="meme-form">
+        <div className="meme-form">
             <div className="input-text">
                 <input type="text" className="top-text" placeholder="Enter top text..."/>
                 <input type="text" className="bottom-text" placeholder="Enter bottom text..."/>
             </div>
-            <button className="get-meme-button">Get a new meme image <FaImage /></button>
-        </form>
+            <button onClick={getRandomURL} className="get-meme-button">Get a new meme image <FaImage /></button>
+        </div>
     )
 }
