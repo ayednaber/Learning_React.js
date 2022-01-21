7b111a2ae61877ea1dbf36487fabb8c7569acc53
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-21 10:21:08 -0500

    Added Form component

---
+++ meme_generator/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { FaImage } from "react-icons/fa"
+
+export default function Form() {
+    return (
+        <div className="meme-form">
+            <div className="input-text">
+                <input type="text" className="top-text" placeholder="Enter top text..."/>
+                <input type="text" className="bottom-text" placeholder="Enter bottom text..."/>
+            </div>
+            <button className="get-meme-button">Get a new meme image <FaImage /></button>
+        </div>
+    )
+}
