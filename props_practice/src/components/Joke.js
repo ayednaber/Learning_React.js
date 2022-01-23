642a84ad35ae573e8f3d11c38752282e387c5b4a
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-23 17:16:26 -0500

    Updated Joke.js to render punchline conditionally based on state

--- props_practice/src/components/Joke.js
+++ props_practice/src/components/Joke.js
@@ -1,10 +1,17 @@
 import React from "react"
 
 export default function Joke(props) {
+    const [isShown, updateIsShown] = React.useState(false)
+
+    function changeIsShown() {
+        updateIsShown(prevIsShown => !prevIsShown)
+    }
+
     return (
         <div className="joke-section">
             <h3>{props.setup}</h3>
-            <p>{props.punchline}</p>
+            <p>{isShown && props.punchline}</p>
+            <button onClick={changeIsShown} className="show-hide">Show/Hide Punchline</button>
             <hr />
         </div>
     )
