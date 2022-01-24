946cf90cb19ed84c02e8dde9d1173b4dff5c7a13
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-24 11:05:02 -0500

    Imported files to meme generator API project

---
+++ meme_generator_api/src/components/Form.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { FaImage } from "react-icons/fa"
+import memesData from "../memesData"
+
+export default function Form() {
+    const memes = memesData.data.memes
+    let randomEntry = memes[Math.floor(Math.random()*memes.length)]
+    const [memeImage, updateMemeImage] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+        allMemeImages: memesData
+    })
+
+
+    function getRandomImage() {
+        randomEntry = memes[Math.floor(Math.random()*memes.length)]
+        updateMemeImage(prevMemeImage => {
+            return {
+                ...prevMemeImage,
+                randomImage: randomEntry.url
+            }
+        })
+    }
+
+    // console.log(memeImage)
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        updateMemeImage(prevMemeImage => ({
+            ...prevMemeImage,
+            [name]: value
+        }))
+    }
+
+    
+    return (
+        <div className="meme-container">
+            <div className="meme-form">
+                <div className="input-text">
+                    <input type="text" className="top-text" placeholder="Enter top text..."
+                        onChange={handleChange}
+                        name="topText"
+                        value={memeImage.topText}
+                    />
+                    <input type="text" className="bottom-text" placeholder="Enter bottom text..."
+                            onChange={handleChange}
+                            name="bottomText"
+                            value={memeImage.bottomText}/>
+                </div>
+                <button onClick={getRandomImage} className="get-meme-button">Get a new meme image <FaImage /></button>
+            </div>
+            <div className="meme">
+                <img src={memeImage.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{memeImage.topText}</h2>
+                <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
+            </div>
+        </div>
+        
+    )
+}
---
+++ meme_generator_api/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png"  className="trollface"/>
+            <h1 className="header-title">Meme Generator</h1>
+        </div>
+    )
+}
