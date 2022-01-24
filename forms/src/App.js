43320c55533b48265454d0ae99b861c080b169ab
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-23 19:48:02 -0500

    Added simple form to App.js and handled changes using onChange event listener

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function Form() {
+  const [firstName, setFirstName] = React.useState("")
+  
+  console.log(firstName)
+
+  function handleChange(event) {
+      setFirstName(event.target.value)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <form>
+          <input
+              type="text"
+              placeholder="First Name"
+              onChange={handleChange}
+          />
+      </form>
+  )
 }
-
-export default App;
