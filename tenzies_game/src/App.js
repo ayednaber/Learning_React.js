da79c42f0831a033e24d3f6855d6b4fdd3dec6f8
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-25 10:38:51 -0500

    Added function to generate die numbers

--- tenzies_game/src/App.js
+++ tenzies_game/src/App.js
@@ -2,6 +2,17 @@ import React from "react"
 import Die from "./components/Die";
 
 function App() {
+  // Function to generate 10 random numbers for the die
+  function allNewDice() {
+    const newDie = []
+    for (let i = 0; i < 10; i++) {
+      const newNum = Math.floor(Math.random() * 6 + 1)
+      newDie.push(newNum)
+    }
+    console.log(newDie)
+  }
+
+  allNewDice()
   return (
     <main>
       <h1 className="title">Tenzies</h1>
