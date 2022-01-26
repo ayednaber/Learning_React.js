aafaa8759521790b9d7b2af65567b1ce4dac2db9
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-25 21:07:15 -0500

    Added starter screen for Quiz

--- solo_project3/src/App.js
+++ solo_project3/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Quiz from "./components/Quiz";
+import Start from "./components/Start";
 
 function App() {
-  return (
-    <h1>Quiz App</h1>
-  );
+  // Creating state for startQuiz
+  const [startQuiz, setStartQuiz] = React.useState(false)
+
+  function start() {
+    setStartQuiz(prevStartQuiz => !prevStartQuiz)
+    console.log("The quiz has started!")
+  }
+
+  if (!startQuiz) {
+    return <Start handleClick={start} />
+  } else {
+    return <Quiz />
+  }
 }
 
 export default App;
