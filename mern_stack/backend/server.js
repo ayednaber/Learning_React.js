7bd1a141c87e649b5d5f2a36159aecc5785813e0
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-28 14:03:38 -0500

    Started developing the backend (express application)

---
+++ mern_stack/backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import restaurants from "./api/restaurants.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json()) // This line is important because if our server receives GET or POST requests, it can convert it into JSON basically.
+
+app.use('/api/v1/restaurants', restaurants)
+app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+
