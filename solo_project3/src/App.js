ec62dedb994bdafa1a67df68d078ac01839b5843
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-27 22:33:09 -0500

    Quiz functionality working, but needs to be styled

--- solo_project3/src/App.js
+++ solo_project3/src/App.js
@@ -12,10 +12,17 @@ function App() {
   // State for questions
   const [questions, setQuestions] = React.useState([])
 
+  const [correctAnswerCount, setCorrectAnswerCount] = React.useState(0)
+
+  // State for if user is done with quiz or not
+  const [quizDone, setQuizDone] = React.useState(false)
+
   function start() {
     setStartQuiz(prevStartQuiz => !prevStartQuiz)
     console.log("The quiz has started!")
   }
+
+  console.log(questions)
 
   React.useEffect(function() {
     console.log("Effect ran")
@@ -23,35 +30,69 @@ function App() {
     fetch(APIURL)
     .then(res => res.json())
     .then(data => data.results.map((keyName, i) => {
-      const allAnswers = keyName.incorrect_answers
+      const allAnswers = [...keyName.incorrect_answers]
       allAnswers.push(keyName.correct_answer)
       const quesObj = {
         key: i,
         question: keyName.question,
         allAnswers: allAnswers,
         correct_answer: keyName.correct_answer,
-        incorrect_answers: keyName.incorrect_answers
+        incorrect_answers: keyName.incorrect_answers,
+        userSelectedAnswer: ""
       }
       questionArray.push(quesObj)
     }))
     setQuestions(questionArray)
   }, [])
 
-  function convertUnicode(input) {
-    return input.replace(/\\u[0-9a-fA-F]{4}/g,function(a,b) {
-      var charcode = parseInt(b,16);
-      return String.fromCharCode(charcode);
-    });
+  function chooseAnswer(answer, question_id) {
+    const questionsArray = []
+    const q_obj = questions[question_id]
+    for (let i = 0; i < questions.length; i++) {
+      if (i === question_id) {
+        const ques_obj = {
+          key: question_id,
+          question: q_obj.question,
+          allAnswers: q_obj.allAnswers,
+          correct_answer: q_obj.correct_answer,
+          incorrect_answers: q_obj.incorrect_answers,
+          userSelectedAnswer: answer
+        }
+        questionsArray.push(ques_obj)
+      } else {
+        questionsArray.push(questions[i])
+      }
+    }
+    setQuestions(questionsArray)
+  }
+
+  function markQuiz() {
+    let correctAnswers = 0;
+    for (let i = 0; i < questions.length; i++) {
+      if (questions[i].userSelectedAnswer === questions[i].correct_answer) {
+        correctAnswers++;
+      }
+    }
+    setQuizDone(true)
+    setCorrectAnswerCount(correctAnswers)
+    return correctAnswers
   }
 
   const questionElements = questions.map((item) => (
-    <Question 
-      key={item.key}
-      question={item.question}
-      correct_answer={item.correct_answer}
-      incorrect_answers={item.incorrect_answers}
-      allAnswers={item.allAnswers}
-    />
+    <div key={item.key} className="question">
+      <h2 className="question-text" dangerouslySetInnerHTML={{ __html: item.question}}></h2>
+      <div className="answers">
+      {item.allAnswers.map((ans, index) => (
+        <button
+          key={index}
+          className="ans"
+          dangerouslySetInnerHTML={{ __html: ans}}
+          onClick={() => chooseAnswer(ans, item.key)}
+          name={ans}
+        ></button>
+      ))}
+      </div>
+    </div>
   ))
   
   if (!startQuiz) {
@@ -60,7 +101,8 @@ function App() {
     return (
       <div className="questions-container">
         {questionElements}
-        <button className="button-quiz check-answers">Check Answers!</button>
+          {!quizDone && <button onClick={markQuiz} className="button-quiz check-answers">Check Answers!</button>}
+          {quizDone && <h3 className="end-quiz">You scored {correctAnswerCount}/5 in the quiz!</h3>}
       </div>
     )
   }
--- solo_project3/src/components/Question.js
+++ solo_project3/src/components/Question.js
@@ -2,33 +2,60 @@ import { nanoid } from "nanoid";
 import React from "react";
 
 export default function Question(props) {
-    const [selectedAnswer, setSelectedAnswer] = React.useState("")
+    const [selectedAnswer, setSelectedAnswer] = React.useState({
+        selAnswer: "",
+        answer1: props.allAnswers[0],
+        answer2: props.allAnswers[1],
+        answer3: props.allAnswers[2],
+        answer4: props.allAnswers[3],
+        correct_answer: props.correct_answer,
+        isCorrect: false
+    })
 
-    function chooseAnswer(a) {
-        setSelectedAnswer(a)
+    function chooseAnswer(answerSel) {
+        setSelectedAnswer(prevAns => ({
+            ...prevAns,
+            selAnswer: answerSel
+        }))
     }
+    console.log(selectedAnswer)
 
-    function checkAnswer(a) {
-        if (selectedAnswer === props.correct_answer) {
-            console.log("Correct")
-        } else {
-            console.log("Wrong")
-        }
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setSelectedAnswer(prevAns => ({
+            ...prevAns,
+            [name]: value
+        }))
+        console.log(event.target.value)
+      }
+
+    function checkAnswer() {
+        console.log(selectedAnswer.selAnswer === props.correct_answer)
     }
 
     return (
         <div className="question">
             <h2 className="question-text" dangerouslySetInnerHTML={{ __html: props.question}}></h2>
             <div className="answers">
-                {props.allAnswers.map(ans => (
-                    <p
-                        className="ans"
-                        key={nanoid()}
-                        dangerouslySetInnerHTML={{ __html: ans}}
-                        onClick={() => chooseAnswer(ans)}
-                    ></p>
-                ))}
+                {props.allAnswers.map(answer => {
+                    return (
+                        <button 
+                            className="ans"
+                            key={nanoid()}
+                            dangerouslySetInnerHTML={{ __html: answer}}
+                            onClick={() => chooseAnswer(answer)}
+                        ></button>
+                    )
+                })}
             </div>
         </div>
     )
 }
+    // {props.allAnswers.map(ans => (
+    //     <p
+    //         className="ans"
+    //         key={nanoid()}
+    //         dangerouslySetInnerHTML={{ __html: ans}}
+    //         onClick={() => chooseAnswer(ans)}
+    //     ></p>
+    // ))}
