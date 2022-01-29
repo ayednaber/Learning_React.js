a50fc179768f23b505566ad22eb1c1656f511644
ayednaber <58320827+ayednaber@users.noreply.github.com>
2022-01-29 11:17:52 -0500

    Added restaurantsDAO.js

---
+++ mern_stack/backend/api/dao/restaurantsDAO.js
@@ -0,0 +1,59 @@
+let restaurants
+
+export default class RestaurantsDAO {
+    static async injectDB(conn) {
+        if (restaurants) {
+            return
+        }
+        try {
+            restaurants = await conn.db(process.env.RESTREVIEWS_NS).collection("restaurants")
+        } catch (e) {
+            console.error (`Unable to establish a connection handle in restaurantsDAO: ${e}`)
+        }
+    }
+
+    static async getRestaurants({
+        filters = null,
+        page = 0,
+        restaurantsPerPage = 20,
+    } = {}) {
+        let query
+        if (filters) {
+            if ("name" in filters) {
+                query = {$text: {$search: filters["name"]}} // Here, we specify that if someone does a text search, then it should search for name. This will be setup later in MongoDB Atlas.
+            } else if ("cuisine" in filters) {
+                query = {"cuisine": {$eq: filters["cuisine"]}}
+            } else if ("zipcode" in filters) {
+                query = {"address.zipcode": {$eq: filters["zipcode"]}}
+            }
+        }
+
+        let cursor
+
+        try {
+            cursor = await restaurants.find(query)
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { restaurantsList: [], totalNumRestaurants: 0}
+        }
+
+        const displayCyrsor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
+
+        try {
+            const restaurantsList = await displayCyrsor.toArray()
+            const totalNumRestaurants = await restaurantsPerPage.countDocuments(query)
+
+            return { restaurantsList, totalNumRestaurants }
+        } catch (e) {
+            console.error(
+                `Unable to convert cursor to array or problem counting doucments, ${e}`
+            )
+            return {restaurantsList: [], totalNumRestaurants: 0}
+        }
+
+
+    }
+
+    
+
+}
